f98dc6ce2658a56fdc9bfc2a14326f14b8774eae
KYEONGWAN KANG <kkw3401@gmail.com>
2022-01-14 19:42:44 +0900

    add: main iconクリック時のイベントを追加
     main iconクリック時、そのindexのmain infoを画面に表示

---
+++ kang's-ramen/src/main/webapp/static/js/index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const main_icons = document.querySelector('.main_icons');
+const main_info_icons = document.querySelectorAll('.main_info_icon');
+const main_infos = document.querySelectorAll('.main_info');
+const logout_icon = document.querySelector('.logout');
+const login_user = document.querySelector('.login_user');
+const reservation_icons = document.querySelectorAll('.reservation');
+const my_team_score = document.querySelector('.my_team_score');
+const opponent_team_score = document.querySelector('.opponent_team_score');
+
+//click main icon
+main_icons.addEventListener('click', (e) => {
+	main_info_icons.forEach((main_info_icon) => {
+		main_info_icon.classList.remove('rotate');
+	});
+	console.log(e.target)
+	let target = e.target;
+	let type = target.dataset.type;
+	if (type == null) {
+		return;
+	}
+	main_infos.forEach((main_info) => {
+		if (type == main_info.dataset.type) {
+			target.classList.add('rotate');
+			main_info.classList.remove('invisible');
+		} else {
+			main_info.classList.add('invisible');
+		}
+	});
+
+});
