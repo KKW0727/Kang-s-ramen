dd597f87ae1a98d567822fa9d4554d11d78f84ac
KYEONGWAN KANG <kkw3401@gmail.com>
2022-01-21 06:01:22 +0900

    add: stripeToken、stripeTokenType、stripeEmail、priceフィールドを追加

---
+++ kang's-ramen/src/main/java/com/spring/kangsRamen/model/json/PaymentVo.java
@@ -0,0 +1,47 @@
+package com.spring.kangsRamen.model.json;
+
+public class PaymentVo {
+
+	private String stripeToken;   
+	private String stripeTokenType;
+	private String stripeEmail;
+	private int price;
+
+	public String getStripeToken() {
+		return stripeToken;
+	}
+
+	public void setStripeToken(String stripeToken) {
+		this.stripeToken = stripeToken;
+	}
+
+	public String getStripeTokenType() {
+		return stripeTokenType;
+	}
+
+	public void setStripeTokenType(String stripeTokenType) {
+		this.stripeTokenType = stripeTokenType;
+	}
+
+	public String getStripeEmail() {
+		return stripeEmail;
+	}
+
+	public void setStripeEmail(String stripeEmail) {
+		this.stripeEmail = stripeEmail;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "PaymentVo [stripeToken=" + stripeToken + ", stripeTokenType=" + stripeTokenType + ", stripeEmail="
+				+ stripeEmail + ", price=" + price + "]";
+	}
+}
