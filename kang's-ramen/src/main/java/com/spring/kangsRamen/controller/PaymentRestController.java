1690bd83875727346892f2edc5883aa34ab0331d
KYEONGWAN KANG <kkw3401@gmail.com>
2022-02-01 00:00:23 +0900

    add: payment, refundメソッドを追加

---
+++ kang's-ramen/src/main/java/com/spring/kangsRamen/controller/PaymentRestController.java
@@ -0,0 +1,30 @@
+package com.spring.kangsRamen.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.kangsRamen.model.json.PaymentVo;
+import com.spring.kangsRamen.service.PaymentService;
+
+@RestController
+public class PaymentRestController {
+
+//	@Autowired
+//	private ReservationService reservationService;
+	@Autowired
+	private PaymentService paymentService;
+
+	@RequestMapping(value = "/payment", method = RequestMethod.POST)
+	public String payment(@RequestBody PaymentVo paymentVo) {
+		return Integer.toString(paymentService.payment(paymentVo));
+	}
+
+	@RequestMapping(value = "/refund", method = RequestMethod.POST)
+	public String refund(@RequestBody PaymentVo paymentVo) {
+		System.out.println(paymentVo);
+		return Integer.toString(paymentService.refund(paymentVo));
+	}
+}
