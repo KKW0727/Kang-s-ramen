f9d518bfc3f2151aa271e3140474b8b96dd40bae
KYEONGWAN KANG <kkw3401@gmail.com>
2022-02-03 14:08:24 +0900

    add: getGameInfoメソッドを追加

--- kang's-ramen/src/main/java/com/spring/kangsRamen/controller/IndexController.java
+++ kang's-ramen/src/main/java/com/spring/kangsRamen/controller/IndexController.java
@@ -1,15 +1,28 @@
 package com.spring.kangsRamen.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.spring.kangsRamen.service.GameInfoService;
 
 @Controller
 public class IndexController {
+
+	@Autowired
+	private GameInfoService gameInfoService;
 
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String index() {
 		System.out.println(123);
 		return "index/index";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/game", method = RequestMethod.GET)
+	public Object getGameInfo() {
+		return gameInfoService.getGameInfo();
 	}
 }
